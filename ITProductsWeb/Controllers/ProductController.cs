756ac0dcd4743541aa53c8630c6dca5226257878
senia1 <oks314@gmail.com>
2023-06-27 12:08:02 +0300

    implemented CRUD Method "Delete"

--- ITProductsWeb/Controllers/ProductController.cs
+++ ITProductsWeb/Controllers/ProductController.cs
@@ -72,11 +72,46 @@ namespace ITProductsWeb.Controllers
             }
             if (ModelState.IsValid)
             {
-                _db.Products.Add(obj);
+                _db.Products.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var productFromDb = _db.Products.Find(id);
+            //var productFromDbFirst = _db.Products.FirstOrDefault(u => u.Id == id);
+            //var productFromDbSingle = _db.Products.SingleOrDefault(u => u.Id == id);
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(productFromDb);
+        }
+
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Products.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Products.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
