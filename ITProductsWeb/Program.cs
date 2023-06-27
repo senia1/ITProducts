a78259f3acb08159f522198707764a45ec258687
senia1 <oks314@gmail.com>
2023-06-27 09:44:33 +0300

    changed Layout.cshtml

--- ITProductsWeb/Program.cs
+++ ITProductsWeb/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddDbContext<ProductsDbContext>(options => options.UseSqlServer
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
+//builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
