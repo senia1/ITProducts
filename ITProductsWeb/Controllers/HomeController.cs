f64523a10a3520113f7e8aca5930e0471e8bb198
senia1 <oks314@gmail.com>
2023-06-27 06:45:22 +0300

    Add project files.

---
+++ ITProductsWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using ITProductsWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ITProductsWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ITProductsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITProductsWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
