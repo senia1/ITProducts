acfce2364d4d4be98a0a266e62a6731d0f11a835
senia1 <oks314@gmail.com>
2023-06-27 08:08:13 +0300

    add ProductController.cs and View Index Product

---
+++ ITProductsWeb/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITProductsWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
