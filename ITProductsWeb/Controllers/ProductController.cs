b806adf52087df3159dfaf89d365be16725ab7c4
senia1 <oks314@gmail.com>
2023-06-27 08:46:39 +0300

    add constructor in ProductController.cs and objProductList

--- ITProductsWeb/Controllers/ProductController.cs
+++ ITProductsWeb/Controllers/ProductController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ITProductsWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ITProductsWeb.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly ProductsDbContext _db;
+
+        public ProductController(ProductsDbContext db)
+        {
+            _db = db;  
+        }
         public IActionResult Index()
         {
+            var objProductList = _db.Products.ToList();
             return View();
         }
     }
